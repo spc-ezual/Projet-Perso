9735a3950880f2b0b2134c714df78577ca50622e
Marcus <70955436+spc-ezual@users.noreply.github.com>
2021-11-15 10:00:36 +0100

    Add files via upload

--- MandJulia.java
+++ MandJulia.java
@@ -1,7 +1,10 @@
 import outils.*;
 public class MandJulia {
     public static void main(String[] args) {
-        creation_mandelbrot_carré(new int[]{20000,20000}, -2, -1.5, 1, 1.5, 500, "mand");
+        //creation_mandelbrot_carré(new int[]{3000,3000}, -2, -1.5, 1, 1.5, 500, "mand");
+        creation_mandelbrot_cosh(new int[]{500,500}, -2, -2, 2, 2, 500,"cosh");
+        creation_mandelbrot_sin(new int[]{500,500}, -2, -2, 2, 2, 500,"sin");
+        creation_mandelbrot_sinh(new int[]{500,500}, -2, -2, 2, 2, 500,"sinh");
     }
     public static int quadratique(complexe z,complexe constante,int pressision){
         int i=0,rep=1;
@@ -40,6 +43,111 @@ public class MandJulia {
         for(int i =0 ; i<taille[0];i++){
             for(int j =0;j<taille[1];j++){
                 couleur=coul(quadratique(new complexe(0,0),affixe(new int[]{i,j},taille,debut_x,fin_x,debut_y,fin_y),pressision));
+                mand.set(i, j, new Pixel((int)couleur[0],(int)couleur[1], (int)couleur[2]));
+            }
+        }
+        BitMap.enregistreBitMap(mand, nom);
+    }
+    public static int cosin(complexe constante,int pressision){
+        if(constante.estReel()&&constante.estImaginaire())return 271;
+        int i=0 ,rep=1;
+        complexe z= constante;
+        while(z.norme()<50&&rep<=pressision){
+            i=0;
+            rep++;
+            while(z.norme()<50&&i<271){
+                z=z.div(constante).cosinus();
+                i++;
+            }
+        }
+        return i;
+    }
+    public static void creation_mandelbrot_cos(int[] taille, double debut_x, double debut_y, double fin_x, double fin_y,int pressision,String nom){
+        BitMap mand=new BitMap(taille[0], taille[1]);
+        pressision=pressision/271+1;
+        double couleur[];
+        for(int i =0 ; i<taille[0];i++){
+            for(int j =0;j<taille[1];j++){
+                couleur=coul(cosin(affixe(new int[]{i,j},taille,debut_x,fin_x,debut_y,fin_y),pressision));
+                mand.set(i, j, new Pixel((int)couleur[0],(int)couleur[1], (int)couleur[2]));
+            }
+        }
+        BitMap.enregistreBitMap(mand, nom);
+    }
+    public static int cosinh(complexe constante,int pressision){
+        if(constante.estReel()&&constante.estImaginaire())return 271;
+        int i=0 ,rep=1;
+        complexe z= constante;
+        while(z.norme()<50&&rep<=pressision){
+            i=0;
+            rep++;
+            while(z.norme()<50&&i<271){
+                z=z.div(constante).cosinusH();
+                i++;
+            }
+        }
+        return i;
+    }
+    public static void creation_mandelbrot_cosh(int[] taille, double debut_x, double debut_y, double fin_x, double fin_y,int pressision,String nom){
+        BitMap mand=new BitMap(taille[0], taille[1]);
+        pressision=pressision/271+1;
+        double couleur[];
+        for(int i =0 ; i<taille[0];i++){
+            for(int j =0;j<taille[1];j++){
+                couleur=coul(cosinh(affixe(new int[]{i,j},taille,debut_x,fin_x,debut_y,fin_y),pressision));
+                mand.set(i, j, new Pixel((int)couleur[0],(int)couleur[1], (int)couleur[2]));
+            }
+        }
+        BitMap.enregistreBitMap(mand, nom);
+    }
+    
+    public static int sinu(complexe constante,int pressision){
+        if(constante.estReel()&&constante.estImaginaire())return 271;
+        int i=0 ,rep=1;
+        complexe z= constante;
+        while(z.norme()<50&&rep<=pressision){
+            i=0;
+            rep++;
+            while(z.norme()<50&&i<271){
+                z=z.div(constante).sinus();
+                i++;
+            }
+        }
+        return i;
+    }
+    public static void creation_mandelbrot_sin(int[] taille, double debut_x, double debut_y, double fin_x, double fin_y,int pressision,String nom){
+        BitMap mand=new BitMap(taille[0], taille[1]);
+        pressision=pressision/271+1;
+        double couleur[];
+        for(int i =0 ; i<taille[0];i++){
+            for(int j =0;j<taille[1];j++){
+                couleur=coul(sinu(affixe(new int[]{i,j},taille,debut_x,fin_x,debut_y,fin_y),pressision));
+                mand.set(i, j, new Pixel((int)couleur[0],(int)couleur[1], (int)couleur[2]));
+            }
+        }
+        BitMap.enregistreBitMap(mand, nom);
+    }
+    public static int sinh(complexe constante,int pressision){
+        if(constante.estReel()&&constante.estImaginaire())return 271;
+        int i=0 ,rep=1;
+        complexe z= constante;
+        while(z.norme()<50&&rep<=pressision){
+            i=0;
+            rep++;
+            while(z.norme()<50&&i<271){
+                z=z.div(constante).sinusH();
+                i++;
+            }
+        }
+        return i;
+    }
+    public static void creation_mandelbrot_sinh(int[] taille, double debut_x, double debut_y, double fin_x, double fin_y,int pressision,String nom){
+        BitMap mand=new BitMap(taille[0], taille[1]);
+        pressision=pressision/271+1;
+        double couleur[];
+        for(int i =0 ; i<taille[0];i++){
+            for(int j =0;j<taille[1];j++){
+                couleur=coul(sinh(affixe(new int[]{i,j},taille,debut_x,fin_x,debut_y,fin_y),pressision));
                 mand.set(i, j, new Pixel((int)couleur[0],(int)couleur[1], (int)couleur[2]));
             }
         }
